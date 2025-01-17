e147d716cf95b01406cc8cfc48a21c3da0b580c2
Khaled Elgendy <khaled.elgendy@trivago.com>
2025-01-17 12:48:25 +0100

    more CMP config

--- config/nvim/lua/_packer.lua
+++ config/nvim/lua/_packer.lua
@@ -51,7 +51,12 @@ require('packer').startup(function(use)
 
     use { -- Autocompletion
         'hrsh7th/nvim-cmp',
-        requires = { 'hrsh7th/cmp-nvim-lsp' },
+        requires = {
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
+        },
     }
 
     use {
