de41831bbfc1657c63b8bae951306bf7eafffd8b
Khaled Elgendy <elgendy1204@gmail.com>
2025-08-03 19:10:45 +0200

    Fix treesitterjk

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -4,31 +4,32 @@ return {
     lazy = false,
     build = ":TSUpdate",
     config = function()
-        require('nvim-treesitter.configs').setup {
-            ensure_installed = { 'go', 'lua', 'typescript', 'vimdoc', 'vim' },
+        require 'nvim-treesitter.configs'.setup {
+            ensure_installed = {
+                "bash",
+                "css",
+                "diff",
+                "dockerfile",
+                "gitcommit",
+                "go",
+                "html",
+                "javascript",
+                "json",
+                "lua",
+                "markdown",
+                "typescript",
+            },
+
+            incremental_selection = { enable = true },
+            highlight = { enable = true },
+            indent = { enable = true },
+            textobjects = { enable = true },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = true,
-
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+            sync_install = false,
             auto_install = true,
-
-            ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-            -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-            indent = { enable = true },
-
-            highlight = {
-                -- `false` will disable the whole extension
-                enable = true,
-
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = false,
-            },
+            ignore_install = {},
+            modules = {},
         }
     end
 }
