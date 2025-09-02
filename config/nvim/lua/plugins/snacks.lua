f0d406065cdd6aafdcb262649cce4625351e0806
Khaled Elgendy <elgendy1204@gmail.com>
2025-09-02 12:11:13 +0200

    Add indent and scroll

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -3,6 +3,8 @@ return {
     priority = 1000,
     lazy = false,
     opts = {
+        indent = { enabled = true },
+        scroll = { enabled = true },
         dashboard = { enabled = true },
     },
 }
