431a7ebd32bf1203db397b59d3b8d1dbee7e38a7
Khaled Elgendy <elgendy1204@gmail.com>
2025-09-02 12:31:11 +0200

    feat(snacks): update plugin options and add notifier keymap
    - Disable scroll and animate features in snacks.nvim configuration.
    - Enable bigfile and notifier options, set notifier timeout to 3000ms.
    - Add keymap to dismiss all notifications with <localleader>un.
    - Suppress undefined-global diagnostics for Snacks.

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -1,10 +1,21 @@
+---@diagnostic disable: undefined-global
+
 return {
     "folke/snacks.nvim",
     priority = 1000,
     lazy = false,
     opts = {
         indent = { enabled = true },
-        scroll = { enabled = true },
+        scroll = { enabled = false },
+        bigfile = { enabled = true },
+        animate = { enabled = false },
         dashboard = { enabled = true },
+        notifier = {
+            enabled = true,
+            timeout = 3000
+        },
+    },
+    keys = {
+        { "<localleader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
     },
 }
