9343abfae052b05833be3f7d7e39ee51e058f147
Khaled Elgendy <elgendy1204@gmail.com>
2025-09-03 17:34:51 +0200

    Handle buffers picker and themes

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -14,10 +14,10 @@ return {
             enabled = true,
             timeout = 3000
         },
-        explorer = { },
+        explorer = {},
         picker = {
             sources = {
-                explorer = { }
+                explorer = {}
             }
         }
     },
@@ -25,7 +25,16 @@ return {
         { "<localleader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
         { "<leader>bd",      function() Snacks.bufdelete() end,     desc = "Delete Buffer" },
 
-        { "<leader>ne",      function() Snacks.explorer() end,            desc = "File Explorer" },
-        { "<c-space>",       function() Snacks.picker.buffers() end,      desc = "Navigate Buffers" },
+        { "<leader>ne",      function() Snacks.explorer() end,      desc = "File Explorer" },
+        {
+            "<c-space>",
+            function()
+                Snacks.picker.buffers({
+                    focus = 'list',
+                    sort_lastused = true,
+                })
+            end,
+            desc = "Navigate Buffers"
+        },
     },
 }
--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -1,31 +1,23 @@
--- gruvbox theme
 return {
-    -- use 'luisiacc/gruvbox-baby'
+    'luisiacc/gruvbox-baby',
     'ellisonleao/gruvbox.nvim',
-    config = function()
-        vim.cmd[[colorscheme gruvbox]]
-    end,
+    {
+        'Mofiqul/vscode.nvim',
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        config = function()
+            vim.o.background = 'dark'
+            vim.cmd [[colorscheme vscode]]
+        end,
+    },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        -- config = function()
+        --     vim.cmd [[colorscheme tokyonight-storm]]
+        -- end,
+    },
 }
-
--- tokyonight theme
--- return {
---     "folke/tokyonight.nvim",
---     lazy = false,
---     priority = 1000,
---     opts = {},
---     config = function()
---         vim.cmd [[colorscheme tokyonight-storm]]
---     end,
--- }
---
--- vscode theme
--- return {
---     'Mofiqul/vscode.nvim',
---     lazy = false,
---     priority = 1000,
---     opts = {},
---     config = function()
---         vim.o.background = 'dark'
---         vim.cmd [[colorscheme vscode]]
---     end,
--- }
