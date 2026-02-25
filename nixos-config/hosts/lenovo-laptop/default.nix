be37930dd48860329d26a5f7a0f44e203a1f0221
Khaled Elgendy <elgendy1204@gmail.com>
2026-02-25 20:54:57 +0100

    More modularity

--- nixos-config/hosts/lenovo-laptop/default.nix
+++ nixos-config/hosts/lenovo-laptop/default.nix
@@ -4,16 +4,8 @@
     imports = [
         ./hardware-configuration.nix
 
-        ../../modules/system/audio.nix
-        ../../modules/system/boot.nix
-        ../../modules/system/docker.nix
-        ../../modules/system/gnome.nix
-        ../../modules/system/locale.nix
-        ../../modules/system/networking.nix
-        ../../modules/system/nix.nix
-        ../../modules/system/packages.nix
-        ../../modules/system/programs.nix
-        ../../modules/system/users.nix
+        ../../modules/system
+        ../../modules/dev
     ];
 
     networking.hostName = "lenovo-laptop";
---
+++ nixos-config/modules/system/default.nix
@@ -0,0 +1,15 @@
+{ ... }:
+
+{
+    imports = [
+        ./audio.nix
+        ./boot.nix
+        ./gnome.nix
+        ./locale.nix
+        ./networking.nix
+        ./nix.nix
+        ./packages.nix
+        ./programs.nix
+        ./users.nix
+    ];
+};
