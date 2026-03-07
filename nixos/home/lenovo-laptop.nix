421c75005068b0f0ccb7f40222b4459e7f16a4e6
Khaled Elgendy <elgendy1204@gmail.com>
2026-03-07 21:07:21 +0100

    Revert "Add desktop settings"
    This reverts commit c7d181426f43db0a3a3e4884dc9f163d15eea843.

--- nixos/home/lenovo-laptop.nix
+++ nixos/home/lenovo-laptop.nix
@@ -1,11 +1,7 @@
 { ... }:
 
 {
-    imports = [
-        ./default.nix
-        ./programs/gnome-settings.nix    # Desktop: swap with ./programs/kde-settings.nix to switch
-        # ./programs/kde-settings.nix
-    ];
+    imports = [ ./default.nix ];
 
     home.username = "khaled";
     home.homeDirectory = "/home/khaled";
