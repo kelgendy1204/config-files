103c9ba1e67ab58ec68270973f314cb4878cd24a
Khaled Elgendy <elgendy1204@gmail.com>
2026-03-07 22:28:57 +0100

    Keyboargst

--- nixos/modules/system/default.nix
+++ nixos/modules/system/default.nix
@@ -4,8 +4,8 @@
     imports = [
         ./audio.nix
         ./boot.nix
-        ./gnome.nix     # Desktop: swap with ./kde.nix to switch
-        # ./kde.nix
+        # ./gnome.nix     # Desktop: swap with ./kde.nix to switch
+        ./kde.nix
         ./locale.nix
         ./networking.nix
         ./nix.nix
