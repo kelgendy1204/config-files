c1b29307b906dcbffe99001c0ff2e9a647451cc3
Khaled Elgendy <elgendy1204@outlook.com>
2026-05-06 14:55:23 +0200

    Sync skills with claude

--- nix/home/shared/programs/agents.nix
+++ nix/home/shared/programs/agents.nix
@@ -5,4 +5,9 @@
         source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/config-files/config/agents";
         recursive = false;
     };
+
+    home.file.".claude/skills" = {
+        source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/config-files/config/agents/skills";
+        recursive = false;
+    };
 }
