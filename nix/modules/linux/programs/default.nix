e3c6123f24db7d1041cf657963a13ebca530b7b9
Khaled Elgendy <elgendy1204@outlook.com>
2026-05-26 12:17:57 +0200

    Fix nx-ld

--- nix/modules/linux/programs/default.nix
+++ nix/modules/linux/programs/default.nix
@@ -1,5 +1,8 @@
 { ... }:
 
 {
+    # Running non-Nix programs with Nix packages
+    programs.nix-ld.enable = true;
+
     programs.firefox.enable = true;
 }
--- nix/modules/shared/programs/default.nix
+++ nix/modules/shared/programs/default.nix
@@ -1,9 +1,6 @@
 { ... }:
 
 {
-    # Running non-Nix programs with Nix packages
-    programs.nix-ld.enable = true;
-
     # Shells
     programs.zsh.enable = true;
     programs.bash.enable = true;
